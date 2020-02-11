2691f303c3872da7b72731e11aed8e91eb4fd4a8
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-02-11 11:11:54 +0100

    Påbörjad lösning, har även experimenterat med GUI:t.

--- src/WebBrowser.java
+++ src/WebBrowser.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -8,10 +9,15 @@ import java.util.Scanner;
 public class WebBrowser {
     public static void main(String[] args) {
         String url = JOptionPane.showInputDialog(null, "where?","The Ultimate Textbrowser",JOptionPane.QUESTION_MESSAGE);
+        int index;
         try {
             URL resource = new URL(url);
             Scanner urlScanner = new Scanner(resource.openStream());
             while (urlScanner.hasNext()) {
+                if (urlScanner.nextLine().contains("href")) {
+                    index = urlScanner.nextLine().indexOf("\"");
+
+                }
                 System.out.println(urlScanner.nextLine());
             }
         } catch (Exception e) {
---
+++ src/browser.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class browser {
+    private JTextField textField1;
+    private JButton goButton;
+    private JPanel panel1;
+}
